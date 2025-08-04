77689bbf4b460a793d25a61786ae3bbf7d07dec8
nada sedki <sedki.nada@gmail.com>
2025-08-04 19:21:29 +0100

    fix: compatibilité LightFM

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import nltk
 from nltk.corpus import stopwords
-import torch
 import seaborn as sns
 import matplotlib.pyplot as plt
 
